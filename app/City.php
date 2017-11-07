cd0f4adeb83df4b52aab944850130322763d672b
Shokhrukh <shohrux.shomaxmudov@gmail.com>
2017-11-07 21:13:19 +0500

    Committed by Shokhrukh

--- app/City.php
+++ app/City.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class City extends Model
 {
+    protected $fillable = [
+        'name'
+    ];
     public function region(){
         return $this->belongsTo('App\Region');
     }
