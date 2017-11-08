e2b5ec17b14c8736bd5a368a090390b4e59bd38f
Shokhrukh <shohrux.shomaxmudov@gmail.com>
2017-11-08 20:03:59 +0500

    Languages

--- routes/web.php
+++ routes/web.php
@@ -13,3 +13,9 @@
 
 Route::get('/', 'Web\WebController@showForm')->name('web.show.form');
 Route::post('/form_submit', 'Web\WebController@submitForm')->name('web.submit.form');
+
+
+Route::get('setlocale/{locale}', function ($locale) {
+    in_array($locale, \Config::get('app.locales'));
+    return redirect()->back()->withCookie(cookie()->forever('language', $locale));
+})->name('lang.switch');
