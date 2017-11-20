85e3be22f1cb7b43ffa1d99d0fecd7e08edb23e9
Shokhrukh <shohrux.shomaxmudov@gmail.com>
2017-11-20 19:31:28 +0500

    Dist css and js

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -6,8 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ config('app.name', 'O\'AU') }}</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css"
-          integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{asset('dist/css/bootstrap.min.css')}}">
     <link href="https://fonts.googleapis.com/css?family=Fira+Sans" rel="stylesheet">
     <style>
         body {
@@ -49,7 +48,7 @@
             </ul>
             <ul class="navbar-nav my-2 my-lg-0">
                 @guest
-                    <li class="nav-item">
+                <li class="nav-item">
                     <a class="nav-link" href="{{ route('login') }}">Кириш</a>
                 </li>
                 @else
@@ -94,22 +93,12 @@
                     <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
                 </div>
             </div>
-
         </div>
     </div>
 
     @yield('content')
 </div>
-
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
-        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
-        crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js"
-        integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh"
-        crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js"
-        integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ"
-        crossorigin="anonymous"></script>
+<script src="{{asset('dist/js/bootstrap.js')}}"></script>
 @if(Session::has('message'))
     <script>
         $("#userModal").modal();
