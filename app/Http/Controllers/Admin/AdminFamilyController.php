9cf606027b5e9baef7e790208a154f040cba0859
Shokhrukh <shohrux.shomaxmudov@gmail.com>
2017-11-28 19:10:35 +0500

    New changes in all

---
+++ app/Http/Controllers/Admin/AdminFamilyController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+use App\Family;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class AdminFamilyController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth:admin');
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('family.family-create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name'=>'required|unique:families'
+        ]);
+        $activity = new Family($request->all());
+        $activity->save();
+        return redirect()->route('admin.index')->with('message','Family created successfully');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        return view('family.family-update')->withFamily(Family::findOrFail($id));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'name'=>'required|unique:families,name,'.$id
+        ]);
+        $activity = Family::findOrFail($id);
+        $activity->update($request->all());
+        return redirect()->route('admin.index')->with('message','Family updated successfully');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $activity =Family::findOrFail($id);
+        $activity->delete();
+        return redirect()->back()->with('message','Family deleted successfully');
+    }
+}
