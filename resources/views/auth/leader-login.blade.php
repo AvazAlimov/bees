adc846a1183a9452f484a2a11afefd9ed52da82b
Sultonbek <s.ikromov@student.inha.uz>
2018-05-03 20:02:36 +0500

    my new changes

--- resources/views/auth/leader-login.blade.php
+++ resources/views/auth/leader-login.blade.php
@@ -7,7 +7,7 @@
                 <div class="panel panel-default">
                     <div class="panel-heading text-center">
                         <img src="{{ asset('Resources/logo1.png') }}" alt="logo" style="width: 96px;">
-                        <h6><b>Leader login</b></h6>
+                        <h4>Лидер кириш саҳифаси</h4>
                     </div>
 
                     <div class="panel-body">
@@ -24,7 +24,7 @@
                             {{ csrf_field() }}
 
                             <div class="form-group{{ $errors->has('username') ? ' has-error' : '' }}">
-                                <label for="username" class="col-md-4 control-label">Username</label>
+                                <label for="username" class="col-md-4 control-label">Логин</label>
 
                                 <div class="col-md-6">
                                     <input id="username" type="text" class="form-control" name="username" value="{{ old('username') }}" required autofocus>
@@ -38,7 +38,7 @@
                             </div>
 
                             <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                                <label for="password" class="col-md-4 control-label">Password</label>
+                                <label for="password" class="col-md-4 control-label">Парол</label>
 
                                 <div class="col-md-6">
                                     <input id="password" type="password" class="form-control" name="password" required>
@@ -55,7 +55,7 @@
                                 <div class="col-md-6 col-md-offset-4">
                                     <div class="checkbox">
                                         <label>
-                                            <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
+                                            <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Еслаб қолиш
                                         </label>
                                     </div>
                                 </div>
@@ -64,7 +64,7 @@
                             <div class="form-group">
                                 <div class="col-md-8 col-md-offset-4">
                                     <button type="submit" class="btn btn-primary">
-                                        Login
+                                        Кириш
                                     </button>
                                     {{--<a class="btn btn-link" href="{{ route('password.request') }}">
                                         Forgot Your Password?
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,7 +30,7 @@
 
                     <!-- Branding Image -->
                     <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
+                        Ўзбекистон Асаларичилари Уюшмаси
                     </a>
                 </div>
 
